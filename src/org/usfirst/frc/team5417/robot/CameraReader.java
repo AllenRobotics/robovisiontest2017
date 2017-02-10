5fb9f7d7bf3f3a7c9143eef254028ed39d3e678e
Eagle Robotics <team5417@gmail.com>
2017-02-09 20:08:22 -0600

    Added ImageScaleOperation to CameraReader

--- src/org/usfirst/frc/team5417/robot/CameraReader.java
+++ src/org/usfirst/frc/team5417/robot/CameraReader.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team5417.robot;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.usfirst.frc.team5417.cv2017.ImageReader;
+import org.usfirst.frc.team5417.cv2017.opencvops.ImageScaleOperation;
 
 import edu.wpi.cscore.CvSink;
 import edu.wpi.first.wpilibj.CameraServer;
@@ -25,9 +26,13 @@ public class CameraReader implements ImageReader {
 	public Mat read() {
 		visionCameraSink.grabFrame(frame);
 		
-		Mat f32f = new Mat();
-		frame.assignTo(f32f, CvType.CV_32FC3);
+		ImageScaleOperation initialScaleOp = new ImageScaleOperation(frame, 320);
+		Mat resizedFrame = initialScaleOp.resize();
 		frame.release();
+		
+		Mat f32f = new Mat();
+		resizedFrame.assignTo(f32f, CvType.CV_32FC3);
+		resizedFrame.release();
 		
 		return f32f;
 	}
